304fdccecdc4bcc5b9e4b4cba695538d716640ab
oncueprod <bjk@oncue1.com>
2025-07-02 08:30:05 -0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: '../dist', // Build to parent directory
+  },
+  root: './src', // Set source root to src
+});
